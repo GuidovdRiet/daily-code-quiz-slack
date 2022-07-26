c3bbd37a7a1c7e6fdda9f06a680bff9dde5ee6ca
Guido van de Riet <guido@chainels.com>
2022-07-26 16:18:34 +0200

    update slackbot

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -3,24 +3,17 @@ import { APIGatewayEvent, Context } from "aws-lambda";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// Bolt framework event listeners
 const expressReceiver = new ExpressReceiver({
   signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
   processBeforeResponse: true,
 });
 
-// Slack App
 const app = new App({
   signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
   token: `${process.env.SLACK_BOT_TOKEN}`,
   receiver: expressReceiver,
 });
 
-/**
- * @description - This function is used to handle the incoming events from Slack
- * @param stringBody
- * @returns
- */
 function parseRequestBody(stringBody: string | null) {
   try {
     return JSON.parse(stringBody ?? "");
@@ -29,7 +22,6 @@ function parseRequestBody(stringBody: string | null) {
   }
 }
 
-// Netlify cloud functions
 export async function handler(event: APIGatewayEvent, context: Context) {
   const payload = parseRequestBody(event.body);
 
